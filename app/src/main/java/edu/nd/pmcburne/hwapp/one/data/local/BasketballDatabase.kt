87e55b7908fe6e96ca5ebae3833fc39a032d9184
julialam071502 <czg9kd@virginia.edu>
2026-03-09 17:19:28 -0400

    Fix BasketballDatabase.kt

--- app/src/main/java/edu/nd/pmcburne/hwapp/one/data/local/BasketballDatabase.kt
+++ app/src/main/java/edu/nd/pmcburne/hwapp/one/data/local/BasketballDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [GameEntity::class], version = 1, exportSchema = false)
+@Database(entities = [GameEntity::class], version = 2, exportSchema = false)
 abstract class BasketballDatabase : RoomDatabase() {
 
     abstract fun gameDao(): GameDao
@@ -20,7 +20,9 @@ abstract class BasketballDatabase : RoomDatabase() {
                     context.applicationContext,
                     BasketballDatabase::class.java,
                     "basketball_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
